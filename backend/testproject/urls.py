b61f367f39bfaee574b9f32540c47bf694c1359f
Awwal001 <75147024+Awwal001@users.noreply.github.com>
2022-05-18 02:22:08 +0100

    Update urls.py

--- backend/testproject/urls.py
+++ backend/testproject/urls.py
@@ -24,7 +24,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('files/', include('file.urls')),
+    path('files/', include('app.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
